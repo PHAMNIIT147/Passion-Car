c4a92e7e434c57f75dc96fa0df48d611ced4dbc1
PHAMNIIT147 <phongpham663@gmail.com>
2019-03-27 11:23:48 +0700

    edit login Vue.js

--- auth/login.js
+++ auth/login.js
@@ -7,15 +7,34 @@
 //     }
 // };
 
-var form = document.getElementById("main-form");
+/*27-3-2019*/
+// var form = document.getElementById("main-form");
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
-    debugger;
-    if (form.username.value == 'phong' && form.password.value == '123') {
-        window.location = "post.html";
-    } else {
-        alert("tai khoan va mat khau cua ban khong hop le");
-        return false;
+// form.addEventListener("submit", function(e) {
+//     e.preventDefault();
+//     if (form.username.value == 'phong' && form.password.value == '123') {
+//         window.location = "post.html";
+//     } else {
+//         alert("tai khoan va mat khau cua ban khong hop le");
+//         return false;
+//     }
+// })
+const app = new Vue({
+    el: '#login',
+    data: {
+        usname: '',
+        psword: '',
+        error: false
+    },
+    methods: {
+        submit: function(e) {
+            e.preventDefault();
+            if (this.usname === 'phong' && this.psword === '123') {
+                window.location.href = 'https://www.w3schools.com/js/js_htmldom_eventlistener.asp';
+                this.error = true;
+            } else {
+                alert('Sai username hoặc password');
+            }
+        }
     }
-})
+});
